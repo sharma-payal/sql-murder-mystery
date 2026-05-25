480af0a595ef77a730766a6f04ae8769e96df7fb
Payal Sharma <payalbhardwaj2020@gmail.com>
2026-05-24 18:42:25 -0700

    Added witness identification and interview investigation

--- investigation_notes.sql
+++ investigation_notes.sql
@@ -9,4 +9,22 @@
 
 SELECT * FROM crime_scene_report WHERE date = 20180115 AND city = 'SQL City';
 
+---2.Query (1st witness and 2nd witness)
+
+--a) First witness:
+
+SELECT * FROM interview WHERE address_street_name = 'Northwestern Dr' ORDER BY address_number DESC;
+
+--b) Second  witness:
+SELECT * FROM interview WHERE name LIKE '%Annable%' AND address_street_name = 'Franklin Ave';
+
+---3. Ivestigate Interview:
+
+--Witness 1 identifies:
+SELECT * FROM interview WHERE person_id = 14887 ;
+
+--Witness 2 identifies:
+SELECT * FROM interview WHERE person_id  = 16371;
+
+
 
